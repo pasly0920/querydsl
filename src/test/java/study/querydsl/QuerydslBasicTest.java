9e0a02cc753576fe07d5f4ba2a0450ab0ecf51e2
pasly0920 <inwoo920621@gmail.com>
2023-01-18 09:17:21 +0900

    test: case문

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -2,6 +2,7 @@ package study.querydsl;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import javax.persistence.EntityManagerFactory;
@@ -304,5 +305,22 @@ public class QuerydslBasicTest {
 
      }
 
+     @Test
+     public void simpleCase() throws Exception {
+         List<String> result = queryFactory
+                 .select(member.age
+                         .when(10).then("열살") .when(20).then("스무살") .otherwise("기타"))
+                 .from(member)
+                 .fetch();
+      }
+
+      @Test
+      public void complexCase() throws Exception {
+          List<String> result = queryFactory
+                  .select(new CaseBuilder()
+                          .when(member.age.between(0, 20)).then("0~20살") .when(member.age.between(21, 30)).then("21~30살") .otherwise("기타"))
+                  .from(member)
+                  .fetch();
+    }
 
 }
